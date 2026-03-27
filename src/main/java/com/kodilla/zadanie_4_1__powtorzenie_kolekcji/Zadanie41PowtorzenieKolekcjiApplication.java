c76ed120644cc8c112587163f36c48514032b228
SlawomirDeja <u0540579977@gmail.com>
2026-03-27 22:32:55 +0100

    Commit1

---
+++ src/main/java/com/kodilla/zadanie_4_1__powtorzenie_kolekcji/Zadanie41PowtorzenieKolekcjiApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.zadanie_4_1__powtorzenie_kolekcji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zadanie41PowtorzenieKolekcjiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zadanie41PowtorzenieKolekcjiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kodilla/zadanie_4_1__powtorzenie_kolekcji/Zadanie41PowtorzenieKolekcjiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.zadanie_4_1__powtorzenie_kolekcji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zadanie41PowtorzenieKolekcjiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
