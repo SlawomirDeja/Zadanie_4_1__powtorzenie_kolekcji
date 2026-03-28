87812c797be9b0e20a9dee3320d9d350d0876958
SlawomirDeja <u0540579977@gmail.com>
2026-03-28 11:59:47 +0100

    Commit1

--- src/main/java/com/kodilla/zadanie_4_1__powtorzenie_kolekcji/Zadanie41PowtorzenieKolekcjiApplication.java
+++ src/main/java/com/kodilla/zadanie_4_1__powtorzenie_kolekcji/Zadanie41PowtorzenieKolekcjiApplication.java
@@ -3,11 +3,28 @@ package com.kodilla.zadanie_4_1__powtorzenie_kolekcji;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.*;
+
 @SpringBootApplication
 public class Zadanie41PowtorzenieKolekcjiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Zadanie41PowtorzenieKolekcjiApplication.class, args);
+
+		List<String> companies = new ArrayList<>();
+		companies.add("DELL");
+		companies.add("APPLE");
+		companies.add("SAMSUNG");
+		companies.add("SONY");
+		companies.add("ALCATEL");
+
+		String concatenatedCompanies = "";
+		for (int i = 0; i < 5; i++) {
+			concatenatedCompanies += companies.get(i);
+			if (i < 4) concatenatedCompanies += ", ";
+		}
+
+		System.out.println(concatenatedCompanies);
 	}
 
 }
